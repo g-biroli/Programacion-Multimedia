abef64ee6f9ea91972c5eb88d5c80c05dc286964
Gabriel Biroli <gabrielbiroli@gmail.com>
2024-11-30 12:55:11 -0300

    Actividad Evaluable Acesso a Datos

--- actividadEvaluable/actividadEvaluable/src/main/java/database/DBConnection.java
+++ actividadEvaluable/actividadEvaluable/src/main/java/database/DBConnection.java
@@ -2,17 +2,20 @@ package database;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class DBConnection {
     //permite llegar a la conexion
 
     private static Connection connection;
 
-    private void createConnection(){
-        String url = "jdbc:mysql://localhost:3306/";
-        connection = DriverManager.getConnection();
+    private void createConnection() throws SQLException {
+
+
+        String url = String.format("jdbc:mysql://%s:%s/%s",SchemaDB.Host, SchemaDB.PORT, SchemaDB.DB_NAME);
+        connection = DriverManager.getConnection(url, "root", "");
     }
-    public Connection getConnection(){
+    public Connection getConnection() throws SQLException {
         if (connection == null){
             createConnection();
         }
---
+++ actividadEvaluable/actividadEvaluable/src/main/java/database/SchemaDB.java
@@ -0,0 +1,11 @@
+package database;
+
+public interface SchemaDB {
+
+    String Host = "127.0.0.1";
+
+    String PORT = "3306";
+
+    String DB_NAME = "contacto";
+
+}
